88d73a3dff8ee2ac7af44096714cbf8cf5a41b95
Alexander Adam <Alexander Adam@DESKTOP-M2DHU9G>
2018-09-16 12:56:39 +0700

    v0.34

--- 175314127_pbo2_20181/src/view/TambahPasienBaruDialog.java
+++ 175314127_pbo2_20181/src/view/TambahPasienBaruDialog.java
@@ -7,6 +7,7 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -17,11 +18,14 @@ import javax.swing.JTextField;
  */
 public class TambahPasienBaruDialog extends JDialog{
     private JLabel judulLabel;
-  private JTextField isiText;
+  private JTextField isiText;   
+  private JComboBox tgl ,bln,thn;
   
  public TambahPasienBaruDialog (String judul){
      this.setTitle(judul); 
      init();
+     text();
+     box();
  }
  public void init(){
      this.setLayout(null);
@@ -31,14 +35,33 @@ public class TambahPasienBaruDialog extends JDialog{
      judulLabel.setFont(new Font("Arial",Font.BOLD,30));
      this.add(judulLabel);
      
-     judulLabel = new JLabel("Nama  :");
+     judulLabel = new JLabel("Nama    :");
      judulLabel.setBounds(150, 200,500,50);
      judulLabel.setFont(new Font ("Arial",Font.BOLD,16));
      this.add(judulLabel);
      
+     judulLabel= new JLabel("Alamat  :");
+     judulLabel.setBounds(150, 250,500,50);
+     judulLabel.setFont(new Font ("Arial",Font.BOLD,16));
+     this.add(judulLabel);        
+         
+     judulLabel = new JLabel("Tanggal Lahir :");
+     judulLabel.setBounds(150,300,500,50);
+     judulLabel.setFont(new Font("Arial",Font.BOLD,16));
+     this.add(judulLabel);
+ }
+ public void text(){
      isiText = new JTextField();
      isiText.setBounds(230,214,100,50);
      isiText.setSize(300, 25);
      this.add(isiText);
+     
+     isiText = new JTextField();
+     isiText.setBounds(230,260,100,50);
+     isiText.setSize(300, 25);
+     this.add(isiText);
+ }
+ public void box(){
+     
  }
 }
